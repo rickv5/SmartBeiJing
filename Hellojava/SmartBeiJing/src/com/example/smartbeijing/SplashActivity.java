dc2fb1dfb70a786efd0d1b8c61be9255416de6ec
rickv5 <515590107@qq.com>
2017-03-28 23:28:36 +0800

    旋转动画完成

--- Hellojava/SmartBeiJing/src/com/example/smartbeijing/SplashActivity.java
+++ Hellojava/SmartBeiJing/src/com/example/smartbeijing/SplashActivity.java
@@ -6,9 +6,17 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
 import android.widget.ImageView;
 
 public class SplashActivity extends Activity {
+
+	private ImageView iv_pic;
+	private AnimationSet as;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -19,17 +27,48 @@ public class SplashActivity extends Activity {
 		initView();//界面
 		
 		initAnimation();//初始化动画
+		
+		startAnimation();//开始动画
+	}
+
+	private void startAnimation() {
+		// TODO Auto-generated method stub
+		//开始播放动画
+		iv_pic.startAnimation(as);
 	}
 
 	private void initAnimation() {
-		// TODO Auto-generated method stub
+		as = new AnimationSet(true);
 		
+		//旋转动画
+		RotateAnimation ra = new RotateAnimation(0, 360, 
+				Animation.RELATIVE_TO_SELF, 0.5f,
+				Animation.RELATIVE_TO_SELF, 0.5f);
+		ra.setDuration(5000);
+		ra.setFillAfter(true);
+		
+		//渐变动画
+		AlphaAnimation aa = new AlphaAnimation(0, 1);
+		aa.setDuration(5000);
+		aa.setFillAfter(true);
+		
+		//缩放动画
+		ScaleAnimation sa = new ScaleAnimation(0, 1, 0, 1,
+				ScaleAnimation.RELATIVE_TO_SELF, 0.5f,
+				ScaleAnimation.RELATIVE_TO_SELF, 0.5f);
+		sa.setDuration(5000);
+		sa.setFillAfter(true);
+		
+		//添加动画到动画集
+		as.addAnimation(sa);
+		as.addAnimation(aa);
+		as.addAnimation(ra);
 	}
 
 	private void initView() {
 		// TODO Auto-generated method stub
 		setContentView(R.layout.activity_splash);
-		ImageView iv_splash_pic = (ImageView) findViewById(R.id.iv_splash_pic);
+		iv_pic = (ImageView) findViewById(R.id.iv_splash_pic);
 	}
 
 }
