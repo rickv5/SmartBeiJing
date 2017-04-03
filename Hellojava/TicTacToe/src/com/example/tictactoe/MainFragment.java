fc44817f53bfee22a7f221cab97670f6baec2eab
rickv5 <515590107@qq.com>
2017-04-03 18:50:10 +0800

    U3T

---
+++ Hellojava/TicTacToe/src/com/example/tictactoe/MainFragment.java
@@ -0,0 +1,47 @@
+package com.example.tictactoe;
+
+import android.app.AlertDialog;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+
+public class MainFragment extends Fragment {
+	private AlertDialog mDialog;
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+							 Bundle savedInstanceState){
+		View rootView = inflater.inflate(R.layout.fragment_main, container,false);
+		//添加处理按钮的代码
+		View aboutButton = rootView.findViewById(R.id.about_button);
+		
+		aboutButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+
+				AlertDialog.Builder buidler = new AlertDialog.Builder(getActivity());
+				buidler.setTitle(R.string.about_title);
+				buidler.setMessage(R.string.about_text);
+				buidler.setCancelable(false);
+			}
+		});
+		return rootView;
+	}
+	
+	@Override
+	public void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		//如果about对话框未关闭，就将其关闭
+		if(mDialog != null) {
+			mDialog.dismiss();
+		}
+	}
+}
+
+
+
