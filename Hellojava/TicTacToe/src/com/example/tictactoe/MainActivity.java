48d4eae25ccb3da04269d2e4d329183446a4ac95
rickv5 <515590107@qq.com>
2017-04-07 09:01:35 +0800

    游戏，第二次提交

--- Hellojava/TicTacToe/src/com/example/tictactoe/MainActivity.java
+++ Hellojava/TicTacToe/src/com/example/tictactoe/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.tictactoe;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+
+	private Button about;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.fragment_main);
+		setContentView(R.layout.activity_main);
 	}
+	
 
 }
--- Hellojava/TicTacToe/src/com/example/tictactoe/MainFragment.java
+++ Hellojava/TicTacToe/src/com/example/tictactoe/MainFragment.java
@@ -1,7 +1,10 @@
 package com.example.tictactoe;
 
+import com.example.tictactoe.utils.PrintOutUtils;
+
 import android.app.AlertDialog;
 import android.app.Fragment;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,20 +16,35 @@ public class MainFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 							 Bundle savedInstanceState){
+		PrintOutUtils.printMessage("1");
 		View rootView = inflater.inflate(R.layout.fragment_main, container,false);
 		//添加处理按钮的代码
 		View aboutButton = rootView.findViewById(R.id.about_button);
 		
+		PrintOutUtils.printMessage("2");
 		aboutButton.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
 
+				PrintOutUtils.printMessage("3");
 				AlertDialog.Builder buidler = new AlertDialog.Builder(getActivity());
 				buidler.setTitle(R.string.about_title);
 				buidler.setMessage(R.string.about_text);
 				buidler.setCancelable(false);
+				buidler.setPositiveButton(R.string.ok_label, new DialogInterface.OnClickListener() {
+					
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						// TODO Auto-generated method stub
+						PrintOutUtils.printMessage("点击了OK");
+					}
+
+
+				});
+				mDialog = buidler.show();
+				PrintOutUtils.printMessage("对话框显示了");
 			}
 		});
 		return rootView;
@@ -36,6 +54,7 @@ public class MainFragment extends Fragment {
 	public void onPause() {
 		// TODO Auto-generated method stub
 		super.onPause();
+		PrintOutUtils.printMessage("4");
 		//如果about对话框未关闭，就将其关闭
 		if(mDialog != null) {
 			mDialog.dismiss();
